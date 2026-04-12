4500c067a4f5c1ca7d7c4336eb913108938832fc
yanji0-0 <yanjstine0.0@gmail.com>
2026-04-12 21:45:34 +0800

    supabase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 import 'app_shell.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://xzirvadcrgqnvrzhekmj.supabase.co', // ← dito yung URL mo
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6aXJ2YWRjcmdxbnZyemhla21qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ5NDY0NDAsImV4cCI6MjA5MDUyMjQ0MH0.5AsrwSkQa2js6VPrupkzwsjH6jGXWEz4X0qE-rDDBbY', // ← dito mo ilalagay yung kinopy mo
+  );
+
   runApp(const AppShell());
 }
